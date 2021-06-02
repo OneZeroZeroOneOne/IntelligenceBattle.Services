6c653efc8351ad743cbca9996a130d2099795512
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2021-06-02 20:07:19 +0300

    fix

--- IntelligenceBattle.Services.GameManager/IntelligenceBattle.Services.GameManager/Program.cs
+++ IntelligenceBattle.Services.GameManager/IntelligenceBattle.Services.GameManager/Program.cs
@@ -20,7 +20,7 @@ namespace IntelligenceBattle.Services.GameManager
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddTransient(x => new PublicContext("Host=185.87.48.116;Database=intelligencebattle;Username=postgres;Password=123123AAA"));
+                    services.AddTransient(x => new PublicContext(Environment.GetEnvironmentVariable("databaseconnectionstring")));
                     services.AddTransient<GameManagerService>();
                     services.AddHostedService<Worker>();
                 });
